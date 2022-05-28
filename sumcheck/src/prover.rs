c509b6a2d5dec18ee624856b52b9703184f8db08
Andreas Bigger <abigger87@gmail.com>
2022-05-28 09:41:28 -0400

    refactoring

--- sumcheck/src/prover.rs
+++ sumcheck/src/prover.rs
@@ -27,5 +27,59 @@ impl Prover {
 		}
 	}
 
+  /// Simulates a Prover calculating the proof.
+  pub fn simulate(&mut self, r: Option<ScalarField>) -> UniPoly {
+    r.is_some().and_then(|r| self.r_vec.push(r.unwrap()));
+    let v = self.g.num_vars() - self.r_vec.len();
+    (0..(2u32.pow(v as u32 - 1))).fold(
+      UniPoly::from_coefficients_vec(vec![(0, 0u32.into())]),
+      |sum, n| sum + self.evaluate_gj(n_to_vec(n as usize, v)),
+    )
+  }
 
+  // Evaluates gj over a vector permutation of points, folding all evaluated terms together into one univariate polynomial
+	pub fn evaluate_gj(&self, points: Vec<ScalarField>) -> UniPoly {
+		cfg_into_iter!(self.g.terms()).fold(
+			UniPoly::from_coefficients_vec(vec![]),
+			|sum, (coeff, term)| {
+				let (coeff_eval, fixed_term) = self.evaluate_term(&term, &points);
+				let curr = match fixed_term {
+					None => UniPoly::from_coefficients_vec(vec![(0, *coeff * coeff_eval)]),
+					_ => UniPoly::from_coefficients_vec(vec![(
+						fixed_term.unwrap().degree(),
+						*coeff * coeff_eval,
+					)]),
+				};
+				curr + sum
+			},
+		)
+	}
+
+  // Evaluates a term with a fixed univar, returning (new coefficent, fixed term)
+	pub fn evaluate_term(
+		&self,
+		term: &SparseTerm,
+		point: &Vec<ScalarField>,
+	) -> (ScalarField, Option<SparseTerm>) {
+		let mut fixed_term: Option<SparseTerm> = None;
+		let coeff: ScalarField =
+			cfg_into_iter!(term).fold(1u32.into(), |product, (var, power)| match *var {
+				j if j == self.r_vec.len() => {
+					fixed_term = Some(SparseTerm::new(vec![(j, *power)]));
+					product
+				}
+				j if j < self.r_vec.len() => self.r_vec[j].pow(&[*power as u64]) * product,
+				_ => point[*var - self.r_vec.len()].pow(&[*power as u64]) * product,
+			});
+		(coeff, fixed_term)
+	}
+
+	// Sum all evaluations of polynomial `g` over boolean hypercube
+	pub fn slow_sum_g(&self) -> ScalarField {
+		let v = self.g.num_vars();
+		let n = 2u32.pow(v as u32);
+		(0..n)
+			.map(|n| self.g.evaluate(&n_to_vec(n as usize, v)))
+			.sum()
+	}
 }
---
+++ sumcheck/src/verifier.rs
@@ -0,0 +1,89 @@
+use ark_bls12_381::Fr as ScalarField;
+
+use ark_ff::Field;
+use ark_poly::polynomial::multivariate::{SparsePolynomial, SparseTerm, Term};
+use ark_poly::polynomial::univariate::SparsePolynomial as UniSparsePolynomial;
+use ark_poly::polynomial::{MVPolynomial, Polynomial};
+use ark_std::cfg_into_iter;
+use rand::Rng;
+
+use crate::common::*;
+
+/// ## Verifier
+///
+/// The verifier is responsible for verifing the proof of summation over the polynomial.
+#[derive(Debug, Clone)]
+pub struct Verifier {
+	pub g: MultiPoly,
+}
+
+impl Verifier {
+  /// Instantiates a new Verifier instance.
+	pub fn new(g: &MultiPoly) -> Self {
+		Prover {
+			g: g.clone(),
+		}
+	}
+
+  /// #### `rnd` [Public Associated Function]
+  ///
+  /// [Public Associated Function]
+  /// Generates a random scalar.
+  pub fn rnd() -> Option<ScalarField> {
+    let mut rng: ThreadRng = rand::thread_rng();
+    let r: ScalarField = rng.gen();
+    Some(r)
+  }
+
+  /// #### `degrees` [Public Associated Function]
+  ///
+  /// Generates a lookup table for max degrees of a polynomial.
+  pub fn degrees(g: &MultiPoly) -> Vec<usize> {
+    let mut lookup: Vec<usize> = vec![0; g.num_vars()];
+    cfg_into_iter!(g.terms()).for_each(|(_, term)| {
+      cfg_into_iter!(term).for_each(|(var, power)| {
+        if *power > lookup[*var] {
+          lookup[*var] = *power
+        }
+      });
+    });
+    lookup
+  }
+
+  /// #### `verify`
+  ///
+  /// Verifies prover's claim
+  /// Presented pedantically:
+  pub fn verify(self, claim: ScalarField) -> bool {
+    // 1st round
+    let mut p = Prover::new(g);
+    let mut gi = p.gen_uni_polynomial(None);
+    let mut expected_c = gi.evaluate(&0u32.into()) + gi.evaluate(&1u32.into());
+    assert_eq!(c_1, expected_c);
+    let lookup_degree = degrees(&g);
+    assert!(gi.degree() <= lookup_degree[0]);
+
+    // middle rounds
+    for j in 1..p.g.num_vars() {
+      let r = Verifier::rnd();
+      expected_c = gi.evaluate(&r.unwrap());
+      gi = p.gen_uni_polynomial(r);
+      let new_c = gi.evaluate(&0u32.into()) + gi.evaluate(&1u32.into());
+      assert_eq!(expected_c, new_c);
+      assert!(gi.degree() <= lookup_degree[j]);
+    }
+    // final round
+    let r = Verifier::rnd();
+    expected_c = gi.evaluate(&r.unwrap());
+    p.r_vec.push(r.unwrap());
+    let new_c = p.g.evaluate(&p.r_vec);
+    assert_eq!(expected_c, new_c);
+    true
+  }
+
+  pub fn slow_verify(g: &MultiPoly, c_1: ScalarField) -> bool {
+    let p = Prover::new(g);
+    let manual_sum = p.slow_sum_g();
+    manual_sum == c_1
+  }
+}
